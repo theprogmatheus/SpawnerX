b07c6571f06e66957ad6383291ccb8f4818d7446
theprogmatheus <theprog.matheus@gmail.com>
2025-07-25 13:58:08 -0300

    Update SpawnerBlock, Create SpawnerBlock listeners

--- src/main/java/com/github/theprogmatheus/mc/plugin/spawnerx/domain/SpawnerBlock.java
+++ src/main/java/com/github/theprogmatheus/mc/plugin/spawnerx/domain/SpawnerBlock.java
@@ -2,6 +2,7 @@ package com.github.theprogmatheus.mc.plugin.spawnerx.domain;
 
 import com.github.theprogmatheus.mc.plugin.spawnerx.util.LinkedObject;
 import org.bukkit.block.Block;
+import org.bukkit.block.CreatureSpawner;
 import org.jetbrains.annotations.NotNull;
 
 public class SpawnerBlock extends LinkedObject<BlockLocationKey> {
@@ -13,4 +14,13 @@ public class SpawnerBlock extends LinkedObject<BlockLocationKey> {
     public Block getBlock() {
         return getOriginal().getBlock();
     }
+
+    public void setup() {
+
+    }
+
+    public static boolean isValidBukkitSpawnerBlock(@NotNull Block block) {
+        return block.getState() instanceof CreatureSpawner;
+    }
+
 }
---
+++ src/main/java/com/github/theprogmatheus/mc/plugin/spawnerx/listener/spawners/SpawnerBlockPlaceListener.java
@@ -0,0 +1,24 @@
+package com.github.theprogmatheus.mc.plugin.spawnerx.listener.spawners;
+
+import com.github.theprogmatheus.mc.plugin.spawnerx.domain.SpawnerBlock;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockPlaceEvent;
+
+public class SpawnerBlockPlaceListener implements Listener {
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    void onBlockPlace(BlockPlaceEvent event) {
+        if (event.isCancelled())
+            return;
+
+        var block = event.getBlock();
+        if (!SpawnerBlock.isValidBukkitSpawnerBlock(block))
+            return;
+
+        new SpawnerBlock(block).setup();
+    }
+
+
+}
