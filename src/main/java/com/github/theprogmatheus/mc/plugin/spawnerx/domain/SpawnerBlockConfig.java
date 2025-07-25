d3a71bd18e45a61ffc8b9d19d502e1a5a5d178fa
theprogmatheus <theprog.matheus@gmail.com>
2025-07-25 15:01:01 -0300

    Create SpawnerBlockConfig

---
+++ src/main/java/com/github/theprogmatheus/mc/plugin/spawnerx/domain/SpawnerBlockConfig.java
@@ -0,0 +1,71 @@
+package com.github.theprogmatheus.mc.plugin.spawnerx.domain;
+
+import lombok.Data;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.block.CreatureSpawner;
+import org.bukkit.entity.EntityType;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.persistence.PersistentDataType;
+import org.bukkit.plugin.Plugin;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+@Data
+public class SpawnerBlockConfig {
+
+    private final transient Plugin plugin;
+    private final transient NamespacedKey namespacedKey;
+
+    private final String id;
+    private final EntityType entityType;
+
+    private String entityName;
+    private int delay = 20;
+    private int minSpawnDelay = 40;
+    private int maxSpawnDelay = 100;
+    private int spawnCount = 4;
+    private int spawnRange = 3;
+    private int requiredPlayerRange = 4;
+    private int maxNearbyEntities = 16;
+
+    public SpawnerBlockConfig(@NotNull Plugin plugin, @NotNull String id, @NotNull EntityType entityType) {
+        this.plugin = plugin;
+        this.namespacedKey = new NamespacedKey(plugin, "spawner_block_config");
+        this.id = id;
+        this.entityType = entityType;
+        this.entityName = entityType.name();
+    }
+
+
+    public ItemStack createItemStack(int amount) {
+        var itemStack = new ItemStack(Material.SPAWNER, amount);
+        var itemMeta = itemStack.getItemMeta();
+        var persistentDataContainer = itemMeta.getPersistentDataContainer();
+
+        persistentDataContainer.set(this.namespacedKey, PersistentDataType.STRING, this.id);
+        itemMeta.setDisplayName("§6Mob Spawner");
+        itemMeta.setLore(List.of(
+                "§7Type: §f" + this.entityName
+        ));
+
+        itemStack.setItemMeta(itemMeta);
+        return itemStack;
+    }
+
+    public boolean updateCreatureSpawner(@NotNull CreatureSpawner creatureSpawner) {
+        creatureSpawner.setSpawnedType(this.entityType);
+
+        creatureSpawner.setDelay(this.delay);
+        creatureSpawner.setMinSpawnDelay(this.minSpawnDelay);
+        creatureSpawner.setMaxSpawnDelay(this.maxSpawnDelay);
+        creatureSpawner.setSpawnCount(this.spawnCount);
+        creatureSpawner.setSpawnRange(this.spawnRange);
+        creatureSpawner.setRequiredPlayerRange(this.requiredPlayerRange);
+        creatureSpawner.setMaxNearbyEntities(this.maxNearbyEntities);
+
+        return creatureSpawner.update();
+    }
+
+}
