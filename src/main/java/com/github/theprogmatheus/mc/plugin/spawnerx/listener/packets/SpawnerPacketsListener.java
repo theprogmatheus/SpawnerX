ce4c3a1ef720477d3b657761ad3777b5439c9de2
theprogmatheus <theprog.matheus@gmail.com>
2025-07-31 18:42:22 -0300

    Finish remove spawner animation

--- src/main/java/com/github/theprogmatheus/mc/plugin/spawnerx/listener/packets/SpawnerPacketsListener.java
+++ src/main/java/com/github/theprogmatheus/mc/plugin/spawnerx/listener/packets/SpawnerPacketsListener.java
@@ -2,9 +2,13 @@ package com.github.theprogmatheus.mc.plugin.spawnerx.listener.packets;
 
 import com.github.retrooper.packetevents.event.PacketListenerAbstract;
 import com.github.retrooper.packetevents.event.PacketSendEvent;
+import com.github.retrooper.packetevents.protocol.nbt.NBTCompound;
+import com.github.retrooper.packetevents.protocol.nbt.NBTString;
 import com.github.retrooper.packetevents.protocol.packettype.PacketType;
 import com.github.retrooper.packetevents.protocol.player.User;
 import com.github.retrooper.packetevents.protocol.world.chunk.Column;
+import com.github.retrooper.packetevents.util.Vector3i;
+import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerBlockEntityData;
 import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerChunkData;
 import com.github.theprogmatheus.mc.plugin.spawnerx.domain.BlockLocationKey;
 import com.github.theprogmatheus.mc.plugin.spawnerx.domain.SpawnerBlock;
@@ -17,8 +21,10 @@ import org.bukkit.block.BlockState;
 import org.bukkit.block.CreatureSpawner;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Arrays;
+import java.util.UUID;
 
 @RequiredArgsConstructor
 public class SpawnerPacketsListener extends PacketListenerAbstract {
@@ -27,13 +33,42 @@ public class SpawnerPacketsListener extends PacketListenerAbstract {
 
     @Override
     public void onPacketSend(PacketSendEvent event) {
-        if (!event.getPacketType().equals(PacketType.Play.Server.CHUNK_DATA))
-            return;
-
         User user = event.getUser();
-        Player player = Bukkit.getPlayer(user.getUUID());
+        if (user == null) return;
+        UUID uniqueId = user.getUUID();
+        if (uniqueId == null)
+            return;
+        Player player = Bukkit.getPlayer(uniqueId);
         if (player == null)
             return;
+
+        if (event.getPacketType().equals(PacketType.Play.Server.CHUNK_DATA))
+            handleChunkDataPacket(event, player);
+        else if (event.getPacketType().equals(PacketType.Play.Server.BLOCK_ENTITY_DATA))
+            handleBlockEntityDataPacket(event, player);
+    }
+
+    private void handleBlockEntityDataPacket(@NotNull PacketSendEvent event, @NotNull Player player) {
+        WrapperPlayServerBlockEntityData packet = new WrapperPlayServerBlockEntityData(event);
+        NBTCompound spawnData = packet.getNBT().getCompoundTagOrNull("SpawnData");
+        if (spawnData == null)
+            return;
+        NBTCompound entity = spawnData.getCompoundTagOrNull("entity");
+        if (entity == null)
+            return;
+        NBTString id = entity.getStringTagOrNull("id");
+        if (id == null || id.getValue() == null || id.getValue().isBlank())
+            return;
+
+        Vector3i position = packet.getPosition();
+        BlockLocationKey blockLocationKey = new BlockLocationKey(player.getWorld().getName(), position.getX(), position.getY(), position.getZ());
+        LinkedObject.getLink(SpawnerBlock.class, blockLocationKey).ifPresent(spawner -> {
+            if (!spawner.isAnimated())
+                event.setCancelled(true);
+        });
+    }
+
+    public void handleChunkDataPacket(@NotNull PacketSendEvent event, @NotNull Player player) {
         World world = player.getWorld();
 
         WrapperPlayServerChunkData packet = new WrapperPlayServerChunkData(event);
