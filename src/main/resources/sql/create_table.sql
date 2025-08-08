586c7ea5638a3a83e29aadca93f9bf9f6ef7e76f
theprogmatheus <theprog.matheus@gmail.com>
2025-08-08 08:12:44 -0300

    Add create_table.sql query

---
+++ src/main/resources/sql/create_table.sql
@@ -0,0 +1,20 @@
+CREATE TABLE %prefix%_spawner_blocks (
+    world VARCHAR(32) NOT NULL,
+    x INT NOT NULL,
+    y SMALLINT NOT NULL,
+    z INT NOT NULL,
+    chunk_x INT NOT NULL,
+    chunk_z INT NOT NULL,
+    config VARCHAR(64) NOT NULL,
+    amount INT NOT NULL DEFAULT 1,
+    PRIMARY KEY (world, x, y, z)
+);
+
+CREATE INDEX idx_chunk ON %prefix%_spawner_blocks (world, chunk_x, chunk_z);
+CREATE INDEX idx_config ON %prefix%_spawner_blocks (config, world);
+
+CREATE TABLE %prefix%_player_profiles (
+    uuid CHAR(36) NOT NULL,
+    data BLOB,
+    PRIMARY KEY (uuid)
+);
