28309da8f37037b4d6bdfcd265fd28ab1c1e9906
theprogmatheus <theprog.matheus@gmail.com>
2025-08-13 19:29:49 -0300

    Add db settings on config

--- src/main/java/com/github/theprogmatheus/mc/plugin/spawnerx/config/env/Config.java
+++ src/main/java/com/github/theprogmatheus/mc/plugin/spawnerx/config/env/Config.java
@@ -22,6 +22,19 @@ public class Config {
     public static final ConfigurationHolder<Boolean> LANG_INDIVIDUAL =
             new ConfigurationHolder<>("lang.individual", Boolean.class);
 
+    public static final ConfigurationHolder<String> DATABASE_TYPE =
+            new ConfigurationHolder<>("database.type", String.class);
+    public static final ConfigurationHolder<String> DATABASE_PREFIX =
+            new ConfigurationHolder<>("database.prefix", String.class);
+    public static final ConfigurationHolder<String> DATABASE_HOST =
+            new ConfigurationHolder<>("database.host", String.class);
+    public static final ConfigurationHolder<String> DATABASE_DBNAME =
+            new ConfigurationHolder<>("database.dbname", String.class);
+    public static final ConfigurationHolder<String> DATABASE_USER =
+            new ConfigurationHolder<>("database.user", String.class);
+    public static final ConfigurationHolder<String> DATABASE_PASS =
+            new ConfigurationHolder<>("database.pass", String.class);
+
     public static final ConfigurationHolder<String> SPAWNERS_ITEM_NAME =
             new ConfigurationHolder<>("spawners.item.name", String.class);
     public static final ConfigurationHolder<List<String>> SPAWNERS_ITEM_LORE =
